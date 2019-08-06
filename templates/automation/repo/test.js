f2d58a6560c2de74b19e2fbeb16f43e3ccf6ed9f
mikementor <mike.mentor@mail.ru>
2019-08-06 11:19:48 +0300

    Create test.js

---
+++ templates/automation/repo/test.js
@@ -0,0 +1,25 @@
+const data = require('./data.js')
+const auth = require('./auth.js')
+
+describe('Feature',()=>{
+
+  before(()=>{
+    // auth
+    //data preparation
+  })
+
+  describe('test', ()=>{
+      //data preparation
+      
+      //action
+      
+      //assert
+      
+  })
+
+  after(()=>{
+  //data cleanup
+    data.cleanup()
+  })
+
+})
